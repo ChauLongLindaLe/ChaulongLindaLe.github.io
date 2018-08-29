be9c4c8152494e5feecc15648bbf315ae103ecf5
Linda Le <linchowmein@hotmail.com>
2018-08-29 13:34:18 +0100

    added scroll reveal and simple layout for portfolio.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
 
+// SCROLL REVEAL
+window.sr = ScrollReveal({ reset: false });
+sr.reveal(
+  '.reveal', {
+    duration: 1000,
+    delay: 200,
+    rotate: { x: 10, y: 0, z: 0 },
+    distance: '50px',
+    easing: 'cubic-bezier(0.445, 0.05, 0.55, 0.95)',
+    mobile: true,
+    viewFactor: 0.4
+  });
 
-
+// SMOOTH SCROLL TO SECTION
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $('a').on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== '') {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
